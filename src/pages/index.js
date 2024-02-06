42eee89c04510254b593e706f3673b224ab0faa3
waddenn <waddenn.github@gmail.com>
2024-02-06 15:28:02 +0100

    index updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,35 +3,35 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const categories = [
-    "sports",
-    "handmade",
-    "fashion",
-    "lighting",
-    "software",
-    "books",
-    "videogames",
-    "toys",
-    "lawn-garden",
-    "musical-instruments",
-    "computers",
-    "hpc",
-    "electronics",
-    "appliances",
-    "officeproduct",
-    "grocery",
-    "dvd",
-    "kitchen",
-    "industrial",
-    "climate-pledge",
-    "music",
-    "hi",
-    "digital-text",
-    "gift-cards",
-    "beauty",
-    "automotive",
-    "mobile-apps",
-    "amazon-devices",
-    "pet-supplies",
+    "Sports et Loisirs", //sports
+    "Fait main", //handmade
+    "Mode", //fashion
+    "Luminaires et Éclairage", //lighting
+    "Logiciels", //software
+    "Livres", //books
+    "Jeux vidéo", //videogames
+    "Jeux et Jouets", //toys
+    "Jardin", //lawn-garden
+    "Instruments de musique et Sono", //musical-instruments
+    "Informatique", //computers
+    "Hygiène et Santé", //hpc
+    "High-Tech", //electronics
+    "Gros électroménager", //appliances
+    "Fournitures de bureau", //officeproduct
+    "Epicerie", //grocery
+    "DVD et Blu-ray", //dvd
+    "Cuisine et Maison", //kitchen
+    "commerce, Industrie et Science", //industrial
+    "Engagement en faveur du climat", //climate-pledge
+    "CD et Vinyles", //music
+    "Bricolage", //hi
+    "Kindle", //digital-text
+    "cartes cadeaux", //gift-cards
+    "Beauté et Parfum", //beauty
+    "Auto et Moto", //automotive
+    "Applis et Jeux", //mobile-apps
+    "Appareils Amazon et Accessoires", //amazon-devices
+    "Animalerie", //pet-supplies
   ];
 
   const handleCategoryClick = (category) => {
