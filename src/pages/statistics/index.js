97c07a49de90282049d1a81ab769a63c6d81b8cb
waddenn <waddenn.github@gmail.com>
2024-02-08 11:33:45 +0100

    new page

---
+++ src/pages/statistics/index.js
@@ -0,0 +1,13 @@
+
+const StatisticsPage = ({ statisticsData }) => {
+  return (
+      <div>
+        <h1>Statistiques des produits</h1>
+        <div>
+          <h2>Articles avec le plus de votes</h2>
+        </div>
+      </div>
+  );
+};
+
+export default StatisticsPage;
