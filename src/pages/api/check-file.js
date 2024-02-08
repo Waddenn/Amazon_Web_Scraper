b08c51c458bfe2c00b4a256d6fad9c1bc8e06fae
waddenn <waddenn.github@gmail.com>
2024-02-08 13:30:03 +0100

    checkfile updated

--- src/pages/api/check-file.js
+++ src/pages/api/check-file.js
@@ -1,19 +1,17 @@
-// pages/api/check-file.js
-import { existsSync, readFileSync, writeFileSync } from 'fs';
-import path from 'path';;
-import { getProductList_json } from '@/fetchmongodb.mjs';
+import { existsSync, writeFileSync, readFileSync } from "fs";
+import path from "path";
+import { getProductList_json } from "@/fetchmongodb.mjs";
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), './top_products_by_category.json');
+  const filePath = path.join(process.cwd(), "./top_products_by_category.json");
 
   try {
-    if (!existsSync(filePath) || readFileSync(filePath, 'utf8').trim().length === 0) {
-      writeFileSync(filePath, JSON.stringify([])); 
+    if (!existsSync(filePath)) {
+      writeFileSync(filePath, JSON.stringify({}));
       await getProductList_json();
-      console.log("Fetching data from Amazon...");
-      res.status(200).json({ message: "Data fetched and file created with an empty array." });
+      res.status(200).json({ message: "Data fetched and file created." });
     } else {
-      res.status(200).json({ message: "File already exists and is not empty." });
+      res.status(200).json({ message: "File already exists." });
     }
   } catch (error) {
     console.error(error);
