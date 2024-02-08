5bd02f58c16c8a5d4e40188047926b35145c0fc8
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2024-02-08 11:56:34 +0100

    fix change path

--- src/db/getProductList.js
+++ src/db/getProductList.js
@@ -1,17 +1,27 @@
 import { readFile } from "node:fs/promises";
 
 const getProductList = async (category, minPrice, maxPrice) => {
-  const json = JSON.parse(await readFile("./top_products_by_category.json", { encoding: "utf-8" }));
-  
-  let list = json.filter(product => product.category === category);
+  const json = JSON.parse(
+    await readFile("/tmp/top_products_by_category.json", { encoding: "utf-8" })
+  );
+
+  let list = json.filter((product) => product.category === category);
 
   if (minPrice) {
-    list = list.filter(product => parseFloat(product.price.replace(/[^0-9,]/g, '').replace(',', '.')) >= parseFloat(minPrice));
+    list = list.filter(
+      (product) =>
+        parseFloat(product.price.replace(/[^0-9,]/g, "").replace(",", ".")) >=
+        parseFloat(minPrice)
+    );
   }
   if (maxPrice) {
-    list = list.filter(product => parseFloat(product.price.replace(/[^0-9,]/g, '').replace(',', '.')) <= parseFloat(maxPrice));
+    list = list.filter(
+      (product) =>
+        parseFloat(product.price.replace(/[^0-9,]/g, "").replace(",", ".")) <=
+        parseFloat(maxPrice)
+    );
   }
-  
+
   return list;
 };
 
