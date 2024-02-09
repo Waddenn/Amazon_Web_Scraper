3b98b82d2527aff048f978f67093904837f3f0a4
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2024-02-09 10:04:37 +0100

    whishlist api

---
+++ src/pages/api/addWhishlist.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const handler = async (req, res) => {
+  if (req.method === "GET") {
+    const uri = process.env.COSMOSDB_CONNECTION_STRING;
+    const client = new MongoClient(uri);
+    try {
+      if (!req.body.user || !req.body.product) {
+        res.status(404).send("error : field not correct");
+        return;
+      }
+      const { user, product } = req.body;
+      await client.connect();
+      const database = client.db("amazon-scrapper");
+      const collection = database.collection("whishlist");
+      await collection.insertOne({ user: user, product: product });
+      res.send("Whislist added");
+    } catch (error) {
+      res.status(400).send(`error : ${error}`);
+    } finally {
+      await client.close();
+    }
+  }
+};
+export default handler;
