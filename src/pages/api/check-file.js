d8235ccafb0d7f7030c074c516142d851a017491
Odecam1 <timotheedanymacedo@gmail.com>
2024-05-02 11:51:38 +0200

    configuration prettier and create scraper folder

--- src/pages/api/check-file.js
+++ src/pages/api/check-file.js
@@ -1,20 +1,20 @@
-import { existsSync, writeFileSync, readFileSync } from "fs";
-import path from "path";
-import { getProductList_json } from "@/fetchmongodb.mjs";
+import { existsSync, writeFileSync, readFileSync } from "fs"
+import path from "path"
+import { getProductList_json } from "@/scraper/fetchmongodb.mjs"
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), "./top_products_by_category.json");
+  const filePath = path.join(process.cwd(), "./top_products_by_category.json")
 
   try {
     if (!existsSync(filePath)) {
-      writeFileSync(filePath, JSON.stringify({}));
-      await getProductList_json();
-      res.status(200).json({ message: "Data fetched and file created." });
+      writeFileSync(filePath, JSON.stringify({}))
+      await getProductList_json()
+      res.status(200).json({ message: "Data fetched and file created." })
     } else {
-      res.status(200).json({ message: "File already exists." });
+      res.status(200).json({ message: "File already exists." })
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error processing your request." });
+    console.error(error)
+    res.status(500).json({ message: "Error processing your request." })
   }
 }
