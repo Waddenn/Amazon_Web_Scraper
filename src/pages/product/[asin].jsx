a6a5a438aeec9a5b75eb94b9db743320ad1a6bf8
waddenn <waddenn.github@gmail.com>
2024-05-03 12:21:25 +0200

    amazon-new

--- src/pages/product/[asin].jsx
+++ src/pages/product/[asin].jsx
@@ -5,8 +5,7 @@ const ProductDetail = () => {
   const router = useRouter()
   const { asin, tp = "1y", cpf = "amazon" } = router.query
 
-  // Constructing the URL with dynamic parameters for period and price type
-  const imageUrl = `https://charts.camelcamelcamel.com/fr/${asin}/${asin.startsWith("XYZ") ? "new.png" : "amazon.png"}?force=1&zero=0&w=1166&h=601&desired=false&legend=1&ilt=1&tp=${tp}&fo=0&lang=en&cpf=${cpf}`
+  const imageUrl = `https://charts.camelcamelcamel.com/fr/${asin}/amazon-new.png?force=1&zero=0&w=1166&h=601&desired=false&legend=1&ilt=1&tp=${tp}&fo=0&lang=en&cpf=${cpf}`
 
   return (
     <div className={styles.productDetail}>
