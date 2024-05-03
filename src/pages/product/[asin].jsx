3cfd416b9c2cddd1cdae1981a5664c90c18b5bb9
waddenn <waddenn.github@gmail.com>
2024-05-03 10:53:05 +0200

    new style

--- src/pages/product/[asin].jsx
+++ src/pages/product/[asin].jsx
@@ -1,15 +1,19 @@
 import { useRouter } from "next/router"
+import styles from "@/styles/ProductDetail.module.css"
 
 const ProductDetail = () => {
   const router = useRouter()
   const { asin } = router.query
 
-  const imageUrl = `https://charts.camelcamelcamel.com/fr/${asin}/amazon.png?force=1&zero=0&w=1166&h=601&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=en`
+  const imageUrl = `https://charts.camelcamelcamel.com/fr/${asin}/${asin.startsWith("XYZ") ? "new.png" : "amazon.png"}?force=1&zero=0&w=1166&h=601&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=en`
 
   return (
-    <div>
-      <h1>Détail du Produit</h1>
-      <img src={imageUrl} alt="Historique des prix" />
+    <div className={styles.productDetail}>
+      <img
+        className={styles.chartImage}
+        src={imageUrl}
+        alt="Historique des prix"
+      />
     </div>
   )
 }
