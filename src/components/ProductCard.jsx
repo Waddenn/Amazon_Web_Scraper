b6b3b6b756a16824652a0281f49049c69433d9ca
waddenn <waddenn.github@gmail.com>
2024-05-06 11:10:14 +0200

    checkWishlist

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,12 @@
+import React, { useState, useEffect } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import Cookies from "js-cookie"
 import styles from "@/styles/ProductCard.module.css"
-import React, { useState, useEffect } from "react"
 import Tippy from "@tippyjs/react"
 import "tippy.js/dist/tippy.css"
 import "tippy.js/themes/light-border.css"
 
-// eslint-disable-next-line max-lines-per-function
 const ProductCard = ({ product }) => {
   const [isInWishlist, setIsInWishlist] = useState(false)
   const [username, setUsername] = useState(null)
@@ -15,12 +14,29 @@ const ProductCard = ({ product }) => {
   useEffect(() => {
     const user = Cookies.get("username")
     setUsername(user)
+    if (user) {
+      checkWishlist(user)
+    }
   }, [])
+
+  const checkWishlist = async (user) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/wishlist?username=${user}`,
+      )
+      const wishlist = await response.json()
+      const productInWishlist = wishlist.some(
+        (item) => item._id === product._id,
+      )
+      setIsInWishlist(productInWishlist)
+    } catch (error) {
+      console.error("Erreur lors de la vérification de la wishlist:", error)
+    }
+  }
 
   const addToWishlist = async () => {
     if (!username) {
       alert("Vous devez être connecté pour ajouter des articles à la wishlist.")
-
       return
     }
 
@@ -34,9 +50,7 @@ const ProductCard = ({ product }) => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({
-            product,
-          }),
+          body: JSON.stringify({ product }),
         },
       )
       const data = await response.json()
