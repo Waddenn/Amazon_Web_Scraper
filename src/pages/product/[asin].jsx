b59477e59840ded16332a99c7b9e6f0b122240e1
Odecam1 <timotheedanymacedo@gmail.com>
2024-05-06 12:13:17 +0200

    +Fix bug when refreshing page

--- src/pages/product/[asin].jsx
+++ src/pages/product/[asin].jsx
@@ -1,10 +1,14 @@
 import { useRouter } from "next/router"
 import styles from "@/styles/ProductDetail.module.css"
 
-const ProductDetail = () => {
-  const router = useRouter()
-  const { asin, tp = "1y" } = router.query
+export const getServerSideProps = (context) => {
+  const { asin } = context.query
 
+  return { props: { asin } }
+}
+const ProductDetail = ({ asin }) => {
+  const router = useRouter()
+  const { tp = "1y" } = router.query
   const imageUrl = `https://charts.camelcamelcamel.com/fr/${asin}/amazon-new.png?force=1&zero=0&w=1166&h=601&desired=false&legend=1&ilt=1&tp=${tp}&fo=0&lang=en`
 
   return (
